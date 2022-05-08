49701e9c51e241cc3654a12fea62bc1fa8e1cf62
Colin Swelin <colin@swelin.com>
2022-05-08 15:43:19 -0400

    test

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -2,4 +2,5 @@
 """ Node classes used by the Python template Node Server. """
 
 from .LaundryNode        import LaundryNode
+from .DishWasherNode        import DishWasherNode
 from .ThinQController    import ThinQController
